d9b32fcc66e2ca662f1efce2ed460dffc43dbbba
Felix Arntz <flixos90@gmail.com>
2025-08-09 14:47:50 -0500

    Implement OpenAI specific logic for parsing model metadata from the API.

--- src/ProviderImplementations/OpenAi/OpenAiModelMetadataDirectory.php
+++ src/ProviderImplementations/OpenAi/OpenAiModelMetadataDirectory.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace WordPress\AiClient\ProviderImplementations\OpenAi;
 
 use RuntimeException;
+use WordPress\AiClient\Files\Enums\FileTypeEnum;
+use WordPress\AiClient\Messages\Enums\ModalityEnum;
 use WordPress\AiClient\Providers\AbstractOpenAiCompatibleModelMetadataDirectory;
+use WordPress\AiClient\Providers\Models\DTO\ModelConfig;
 use WordPress\AiClient\Providers\Models\DTO\ModelMetadata;
+use WordPress\AiClient\Providers\Models\DTO\SupportedOption;
+use WordPress\AiClient\Providers\Models\Enums\CapabilityEnum;
 
 /**
  * Class for the OpenAI model metadata directory.
@@ -35,10 +40,140 @@ class OpenAiModelMetadataDirectory extends AbstractOpenAiCompatibleModelMetadata
                 'Unexpected API response: Missing the data key.'
             );
         }
+
+        // Unfortunately, the OpenAI API does not return model capabilities, so we have to hardcode them here.
+        $gptCapabilities = [
+            CapabilityEnum::textGeneration(),
+            CapabilityEnum::chatHistory(),
+        ];
+        $gptOptions = [
+            new SupportedOption(ModelConfig::KEY_SYSTEM_INSTRUCTION),
+            new SupportedOption(ModelConfig::KEY_CANDIDATE_COUNT),
+            new SupportedOption(ModelConfig::KEY_MAX_TOKENS),
+            new SupportedOption(ModelConfig::KEY_TEMPERATURE),
+            new SupportedOption(ModelConfig::KEY_TOP_P),
+            new SupportedOption(ModelConfig::KEY_STOP_SEQUENCES),
+            new SupportedOption(ModelConfig::KEY_PRESENCE_PENALTY),
+            new SupportedOption(ModelConfig::KEY_FREQUENCY_PENALTY),
+            new SupportedOption(ModelConfig::KEY_LOGPROBS),
+            new SupportedOption(ModelConfig::KEY_TOP_LOGPROBS),
+            new SupportedOption(ModelConfig::KEY_OUTPUT_MIME_TYPE, ['text/plain', 'application/json']),
+            new SupportedOption(ModelConfig::KEY_OUTPUT_SCHEMA),
+            // TODO: Where to put this as a constant?
+            new SupportedOption('functionCalling'),
+        ];
+        $gptMultimodalInputOptions = $gptOptions + [
+            new SupportedOption(
+                // TODO: Where to put this as a constant?
+                'inputModalities',
+                [
+                    [ModalityEnum::text()],
+                    [ModalityEnum::text(), ModalityEnum::image()],
+                    [ModalityEnum::text(), ModalityEnum::image(), ModalityEnum::audio()],
+                ]
+            ),
+        ];
+        $gptMultimodalSpeechOutputOptions = $gptMultimodalInputOptions + [
+            new SupportedOption(
+                ModelConfig::KEY_OUTPUT_MODALITIES,
+                [
+                    [ModalityEnum::text()],
+                    [ModalityEnum::text(), ModalityEnum::audio()],
+                ]
+            ),
+        ];
+        $imageCapabilities = [
+            CapabilityEnum::imageGeneration(),
+        ];
+        $dalleImageOptions = [
+            new SupportedOption(ModelConfig::KEY_CANDIDATE_COUNT),
+            new SupportedOption(ModelConfig::KEY_OUTPUT_MIME_TYPE, ['image/png']),
+            // TODO: Where to put this as a constant?
+            new SupportedOption('outputFileType', [FileTypeEnum::inline(), FileTypeEnum::remote()]),
+            // TODO: Where to put this as a constant?
+            new SupportedOption('imageOrientation', ['square', 'landscape', 'portrait']),
+            // TODO: Where to put this as a constant?
+            new SupportedOption('imageAspectRatio', ['1:1', '7:4', '4:7']),
+        ];
+        $gptImageOptions = [
+            new SupportedOption(ModelConfig::KEY_CANDIDATE_COUNT),
+            new SupportedOption(ModelConfig::KEY_OUTPUT_MIME_TYPE, ['image/png', 'image/jpeg', 'image/webp']),
+            // TODO: Where to put this as a constant?
+            new SupportedOption('outputFileType', [FileTypeEnum::inline()]),
+            // TODO: Where to put this as a constant?
+            new SupportedOption('imageOrientation', ['square', 'landscape', 'portrait']),
+            // TODO: Where to put this as a constant?
+            new SupportedOption('imageAspectRatio', ['1:1', '3:2', '2:3']),
+        ];
+        $ttsCapabilities = [
+            CapabilityEnum::textToSpeechConversion(),
+        ];
+        $ttsOptions = [
+            new SupportedOption(ModelConfig::KEY_OUTPUT_MIME_TYPE, ['audio/mpeg', 'audio/ogg', 'audio/wav']),
+            // TODO: Where to put this as a constant?
+            new SupportedOption('voice'),
+        ];
+
         return array_values(
             array_map(
-                static function (array $modelData): ModelMetadata {
-                    // TODO: Create ModelMetadata object from API data.
+                static function (array $modelData) use (
+                    $gptCapabilities,
+                    $gptOptions,
+                    $gptMultimodalInputOptions,
+                    $gptMultimodalSpeechOutputOptions,
+                    $imageCapabilities,
+                    $dalleImageOptions,
+                    $gptImageOptions,
+                    $ttsCapabilities,
+                    $ttsOptions,
+                ): ModelMetadata {
+                    $modelId = $modelData['id'];
+                    if (
+                        str_starts_with($modelId, 'dall-e-') ||
+                        str_starts_with($modelId, 'gpt-image-')
+                    ) {
+                        $modelCaps = $imageCapabilities;
+                        if (str_starts_with($modelId, 'gpt-image-')) {
+                            $modelOptions = $gptImageOptions;
+                        } else {
+                            $modelOptions = $dalleImageOptions;
+                        }
+                    } elseif (
+                        str_starts_with($modelId, 'tts-') ||
+                        str_contains($modelId, '-tts')
+                    ) {
+                        $modelCaps = $ttsCapabilities;
+                        $modelOptions = $ttsOptions;
+                    } elseif (
+                        (str_starts_with($modelId, 'gpt-') || str_starts_with($modelId, 'o1-'))
+                        && !str_contains($modelId, '-instruct')
+                        && !str_contains($modelId, '-realtime')
+                    ) {
+                        if (str_starts_with($modelId, 'gpt-4o')) {
+                            $modelCaps = $gptCapabilities;
+                            $modelOptions = $gptMultimodalInputOptions;
+                            // New multimodal output model for audio generation.
+                            if (str_contains($modelId, '-audio')) {
+                                $modelOptions = $gptMultimodalSpeechOutputOptions;
+                            }
+                        } elseif (!str_contains($modelId, '-audio')) {
+                            $modelCaps = $gptCapabilities;
+                            $modelOptions = $gptOptions;
+                        } else {
+                            $modelCaps = [];
+                            $modelOptions = [];
+                        }
+                    } else {
+                        $modelCaps = [];
+                        $modelOptions = [];
+                    }
+
+                    return new ModelMetadata(
+                        $modelId,
+                        $modelId, // The OpenAI API does not return a display name.
+                        $modelCaps,
+                        $modelOptions
+                    );
                 },
                 (array) $responseData['data']
             )
