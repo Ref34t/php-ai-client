c026b977dd708744ab95b298f00f5bd6913817af
Felix Arntz <flixos90@gmail.com>
2025-08-29 11:18:09 -0700

    Add polyfills for str_starts_with and str_ends_with.

--- src/polyfills.php
+++ src/polyfills.php
@@ -14,10 +14,10 @@ if (!function_exists('array_is_list')) {
      *
      * An array is considered a list if its keys consist of consecutive numbers from 0 to count($array)-1.
      *
+     * @since n.e.x.t
+     *
      * @param array<mixed> $array The array to check.
      * @return bool True if the array is a list, false otherwise.
-     *
-     * @since n.e.x.t
      */
     function array_is_list(array $array): bool
     {
@@ -34,5 +34,47 @@ if (!function_exists('array_is_list')) {
         }
 
         return true;
+    }
+}
+
+if (!function_exists('str_starts_with')) {
+    /**
+     * Checks if a string starts with a given substring.
+     *
+     * @since n.e.x.t
+     *
+     * @param string $haystack The string to search in.
+     * @param string $needle The substring to search for.
+     * @return bool True if $haystack starts with $needle, false otherwise.
+     */
+    function str_starts_with(string $haystack, string $needle): bool
+    {
+        if ( '' === $needle ) {
+            return true;
+        }
+
+        return 0 === strpos( $haystack, $needle );
+    }
+}
+
+if (!function_exists('str_ends_with')) {
+    /**
+     * Checks if a string ends with a given substring.
+     *
+     * @since n.e.x.t
+     *
+     * @param string $haystack The string to search in.
+     * @param string $needle The substring to search for.
+     * @return bool True if $haystack ends with $needle, false otherwise.
+     */
+    function str_ends_with(string $haystack, string $needle): bool
+    {
+        if ( '' === $haystack ) {
+            return '' === $needle;
+        }
+
+        $len = strlen( $needle );
+
+        return substr( $haystack, -$len, $len ) === $needle;
     }
 }
